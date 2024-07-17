6fd54391ab23b1b02b50be3590fa03068ecc4ace
habib-153 <h.r.sihab155@gmail.com>
2024-07-18 02:47:36 +0600

    feat: Add beforeunload event listener to prompt user before leaving with items in cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import { Toaster } from "sonner";
 import MainLayout from "./components/layouts/MainLayout";
+import { useAppSelector } from "./redux/hooks";
+import { RootState } from "./redux/store";
+import { useEffect } from "react";
 
 const App = () => {
+  const cart = useAppSelector((state: RootState) => state.cart.items);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      if (cart.length > 0) {
+        event.preventDefault();
+        event.returnValue =
+          "You have items in your cart. Are you sure you want to leave?";
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [cart]);
   return (
     <div className="">
       <MainLayout></MainLayout>
-      <Toaster position="top-right"/>
+      <Toaster position="top-right" />
     </div>
   );
-}
+};
 
 export default App;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,7 +3,7 @@ import { baseApi } from "./api/baseApi";
 import storage from "redux-persist/lib/storage";
 import cartReducer from './features/cartSlice'
 import filterReducer from './features/filterSlice'
-import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
 
 const persistConfig = {
   key: 'cart',
@@ -30,3 +30,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
